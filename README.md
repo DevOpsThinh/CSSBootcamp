c81cbbcf0bd56bf3ecd48d6ee7a5a0eb9aea54cc
Nguyen Truong Thinh <nguyentruongthinhvn2020@gmail.com>
2022-05-18 23:57:21 +0700

    Complete beautiful code & generate powerful documentation for our sass code.

---
+++ README.md
@@ -0,0 +1,17 @@
+####  "Start writing some Syntactically Awesome Style Sheets (Sass). Clean, beautiful code, make other dev be able to read what is there & understand it!"<br>
+
+### Commands
+- "node-sass --output-style compressed scss --output css" in assets directory for compile *.scss files into css files; 
+- "npm run build" or "gulp sdoc" for generate sass documentation.<br>
+<img src="img/default_theme.png" height="480" /><br>
+<img src="img/viewsource.png" height="480" /> <br>
+
+### Used Software Info:
+- Visual Studio Code v1.67.1
+- Node.js 16
+- LibSass (with node-sass): a wrapped C/C++ library
+- Toolkit: SassDoc 2.0 from Hugo in Gulp pipelines
+
+### References
+- Sass: https://sass-lang.com/
+- SassDoc: http://sassdoc.com/
--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
-function generateSassDoc(cb) {
-  // place code for your default task here
+/**
+ * Using Gulp pipelines to use SassDoc 2.0 to generate powerful documentation.  
+ * 
+ * @author Nguyen Truong Thinh
+ * @reference http://sassdoc.com/gulp/
+*/
   var gulp = require('gulp');
-  var converter = require('sass-convert');
   var sassdoc = require('sassdoc');
-  var sass = require('gulp-sass');
 
-  gulp.task('docs', function() {
+  gulp.task('sdocs', function() {
     var options = {
       dest: 'docs',
       verbose: true,
@@ -19,19 +21,35 @@ function generateSassDoc(cb) {
       },
       theme: 'default',
       package: './package.json',
-      basePath: 'https://github.com/SassDoc/sassdoc',
+      basePath: 'https://github.com/DevOpsThinh/CSSBootcamp/tree/master/assets/scss',
     };
-    return gulp.src('/assets/scss/**/*.+(scss|sass)')
-    .pipe(converter({
-      from: 'scss',
-      to: 'sass',
-    }))
-    .pipe(sassdoc(options))
-    .pipe(sass())
-    .pipe(gulp.dest('/docs'));
+    return gulp.src('assets/scss/**/*.scss')
+      .pipe(sassdoc(options))
+      .pipe(gulp.dest('/docs'));
   });
 
-  cb();
-}
-
-exports.default = generateSassDoc;
+  // var converter = require('sass-convert');
+  // gulp.task('sconverter', function() {
+  //   var options = {
+  //     dest: 'docs',
+  //     verbose: true,
+  //     display: {
+  //       access: ['public', 'private'],
+  //       alias: true,
+  //       watermark: true,
+  //     },
+  //     groups: {
+  //       'undefined': 'helpers'
+  //     },
+  //     theme: 'default',
+  //     package: './package.json',
+  //     basePath: 'https://github.com/SassDoc/sassdoc',
+  //   };
+  //   return gulp.src('assets/scss/**/*.+(sass|scss)')
+  //     .pipe(converter({
+  //       from: 'sass',
+  //       to: 'scss',
+  //     }))
+  //     .pipe(sassdoc(options))
+  //     .pipe(gulp.dest('/docs'));
+  // });
